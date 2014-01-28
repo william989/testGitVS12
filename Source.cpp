2c51d788fd231c7af427bd9ece10fe0dcc4dacde
william989 <hwilliam.cy@gmail.com>
2014-01-27 19:34:44 -0500

    asdf

--- Source.cpp
+++ Source.cpp
@@ -3,7 +3,7 @@
 int main() 
 {
 std::cout << "esto debe ser una nueva version!\n";
-std::cout << "esto debe ser una nueva version 2!\n";
+std::cout << "esto debe ser una nueva version 21!\n";
 system("PAUSE");
 return 0;
 }
