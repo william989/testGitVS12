395a1a5799ec85d21d906b17b282a60bab2278b7
william989 <hwilliam.cy@gmail.com>
2014-01-27 19:47:31 -0500

    nuevo.cpp
    nuevo commit

--- Source.cpp
+++ Source.cpp
@@ -4,7 +4,6 @@ int main()
 {
 std::cout << "esto debe ser una nueva version!\n";
 std::cout << "esto debe ser una nueva version 2!\n";
-std::cout << "esto debe ser una nueva version 3!\n";
 system("PAUSE");
 return 0;
 }
