e5e3ee0e967bbea457e1ac0c4f8679b83d9d13a3
william989 <hwilliam.cy@gmail.com>
2014-01-27 19:48:58 -0500

    com3

--- Source.cpp
+++ Source.cpp
@@ -4,6 +4,7 @@ int main()
 {
 std::cout << "esto debe ser una nueva version!\n";
 std::cout << "esto debe ser una nueva version 2!\n";
+std::cout << "esto debe ser una nueva version 3!\n";
 system("PAUSE");
 return 0;
 }
